c6f18dfdbf52096038eda112da3195a8ab870b38
EAnnaeva <annaeva@dev.vtb.ru>
2024-02-09 17:50:40 +0300

    Initial commit

---
+++ src/main/java/org/openapitools/api/OpenApiComponent.java
@@ -0,0 +1,15 @@
+package org.openapitools.api;
+
+import com.fasterxml.jackson.databind.Module;
+import org.openapitools.jackson.nullable.JsonNullableModule;
+
+import org.springframework.context.annotation.Bean;
+
+public class OpenApiComponent {
+
+    @Bean(name = "org.openapitools.api.OpenApiGeneratorApplication.jsonNullableModule")
+    public Module jsonNullableModule() {
+        return new JsonNullableModule();
+    }
+
+}
